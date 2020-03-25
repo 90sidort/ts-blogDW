ffe6619cd72a39d6cca9c73137e431daba9eb4b9
Andrew Andrewsson <dabroszynwielki@gmail.com>
2020-03-25 14:37:20 +0100

    add title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 const indexPage = () => {
     return (
             <Layout>
+                <Head />
                 <h1>Cześć!</h1>
                 <p>Ta strona jest poświęcona serii "Dasz Wiarę", pierwotnie publikowanej na <a class="linkUnderscore" href="https://www.wykop.pl/tag/daszwiare/" target="_blank" rel="noopener noreferrer">Wykopie</a>.</p>
                 <p>Co to za seria i o czym jest ten blog? Sprawdź zakładkę <Link class="linkUnderscore" to='/about'>O serii</Link>.</p>
