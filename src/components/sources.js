57e617656a9d40805672507be51bfa8da54580ab
Andrew Andrewsson <sidor@adamed.com>
2020-05-23 14:30:14 +0200

    sourcesButton

--- src/components/sources.js
+++ src/components/sources.js
@@ -1,28 +1,43 @@
 import React from 'react'
 import sourcesStyles from '../styles/sources.module.scss'
 
-const Sources = (props) => {
-    const sourceTable = props.sources.split("   ")
-    return (
-        <div className={sourcesStyles.sourceDiv}>
-            <p>
-                Źródła:
-            </p>
-            <ol>
-                {sourceTable.map((source) => {
-                    if(source.includes('http')) {
-                        return (
-                            <li><a className={sourcesStyles.sourceLink} href={source} target="_blank" rel="noopener noreferrer">{source}</a></li>
-                        )
-                    } else {
-                        return (
-                            <li>{source}</li>
-                        )
+class Sources extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { show: false }
+    }
+    showSources = (e) => {
+        const isShown = this.state.show === false ? true : false
+        this.setState(() => ({ show: isShown}))
+    }
+    generateSources = () => {
+        return this.props.sources.split("   ")
+    }
+    render() {
+        return (
+            <div className={sourcesStyles.sourceDiv}>
+                <button className={sourcesStyles.sourceButton} onClick={this.showSources}>
+                    ŹRÓDŁA
+                </button>
+                {this.state.show === true &&
+                    <ol className={sourcesStyles.sourceList}>
+                        {this.generateSources().map((source) => {
+                            if(source.includes('http')) {
+                                return (
+                                    <li><a className={sourcesStyles.sourceLink} href={source} target="_blank" rel="noopener noreferrer">{source}</a></li>
+                                )
+                            } else {
+                                return (
+                                    <li>{source}</li>
+                                )
+                            }
+                        })
                     }
-                })}
-            </ol>
-        </div>
-    )
+                    </ol>
+                }
+            </div>
+        )
+    }
 }
 
 export default Sources
