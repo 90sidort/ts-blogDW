433923515779f2f22c1dff7df130be1701318c43
unknown <90sidort@gmail.com>
2020-06-12 17:03:58 +0200

    added tags to gatsby-node

---
+++ src/templates/tags-page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+const TagsPage = () => {
+    return (
+        <Layout>
+            <Head title="Tag"/>
+                <h1>Test</h1>
+        </Layout>
+    )
+}
+
+export default TagsPage
